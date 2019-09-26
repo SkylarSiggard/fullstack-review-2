ca254c2e81601b90b3410f5e4998c64a8828b950
skylar siggard <siggardskylar@gmail.com>
2019-09-26 17:13:22 -0600

    now its done

--- src/components/Catalog/Game.js
+++ src/components/Catalog/Game.js
@@ -10,9 +10,9 @@ export default class Game extends Component {
     render() {
         return(
         <div className="game">
-            {/* <h3>{this.props.data.title}</h3>
-      <img src={this.props.data.img} alt={this.props.data.title}/>
-      <h4>{this.props.data.price}</h4> */}
+            <h3>{this.props.data.title}</h3>
+      <img src={this.props.data.image} alt={this.props.data.title}/>
+      <h4>{this.props.data.price}</h4>
         </div>
         )
     }
